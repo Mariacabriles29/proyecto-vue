97aec4279e556ec93d95d87e88ed4c933a487c26
Mariacabriles29 <mecp2992@gmail.com>
2025-01-30 11:21:13 -0400

    feat: create Route for the views

---
+++ src/components/ListTask.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Lista de tareas</h2>
+    <input type="text" v-model="newTask" @keyup.enter="addTask" >
+    <ul>
+      <li v-for="(tasks,index) in tasks" :key="index">
+        {{ tasks }}
+        <button @click="deleteTask(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import {ref} from 'vue';
+const tasks= ref([]);
+const newTask= ref('');
+const addTask = () => {
+  tasks.value.push(newTask.value);
+  newTask.value='';
+}
+//se crea una funcion para eliminar una tarea
+const deleteTask = (index) => {
+  tasks.value.splice(index,1);  }
+
+ </script>
+<style></style>
